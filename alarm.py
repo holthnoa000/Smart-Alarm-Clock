c8503397218430dbbe00319dacd3db7610848fa7
holthnoa000 <32803806+holthnoa000@users.noreply.github.com>
2018-12-30 12:27:27 -0600

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -14,24 +14,32 @@ pygame.mixer.music.load("default.mp3")
 
 
 def alarm():
+    count = 0
     
     while True:
         current_time = time.strftime("%I:%M")
         ampm = time.strftime("%p")
         input_state = GPIO.input(18)
-        count = 0
+        #print (count)
         
-        if current_time == "12:42" and ampm == "PM" and input_state == True and count == 0:
+        if current_time == "12:24" and ampm == "PM" and input_state == True and count == 0:
             if pygame.mixer.music.get_busy() == True:
                 continue
             else:
                 pygame.mixer.music.play()
                 print(current_time , ampm)
+                GPIO.output(14, True)
         
-        if input_state == False:
+        if input_state == False: #snooze button
             pygame.mixer.music.stop()
             GPIO.output(14, False)
             count+=1
-            break
+            
+        elif count >= 1: #if the button has been pressed, pause the program for one minute and then reset the button state back to 0.
+            time.sleep(60)
+            count = 0
         
-        time.sleep(0.25)
+        time.sleep(0.2)
+        
+    
+alarm() 
